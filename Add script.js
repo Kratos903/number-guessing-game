40e2461df705e76febcf70578fd20c957c4cac35
Kratos903 <193199832+Kratos903@users.noreply.github.com>
2025-12-31 15:41:09 +0530

    Add description comments to game code

--- Add script.js
+++ Add script.js
@@ -2,3 +2,5 @@ function sayHello() {
   document.getElementById("message").innerText =
     "Hello! You clicked the button 🎉";
 }
+// Number Guessing Game
+// This program lets the user guess a random number
